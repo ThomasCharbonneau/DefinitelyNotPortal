afbd6d50427efb5be48e596c5981aaaa1253f056
AjPereira04 <armandojrpereira97@hotmail.com>
2018-02-05 22:39:05 -0500

    MenuManqueCode

---
+++ Assets/Scripts/GradateurControlleur.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GradateurControlleur : MonoBehaviour {
+
+    public bool ValeurMiseÀJour { get; set; }
+    public int Valeur { get; set; }
+
+    public Slider gradateur { get; set; }
+    Text txtValeur;
+
+    public GameObject PnlText;
+    int vitesseBase = 60;
+    
+	
+	void Start () {
+        gradateur = GetComponentInChildren<Slider>();
+        gradateur.value = vitesseBase;
+        Valeur = (int)gradateur.value;
+
+        txtValeur = PnlText.GetComponentInChildren<Text>();
+        txtValeur.text = Valeur.ToString();
+        ValeurMiseÀJour = true;
+	}
+	
+	
+
+    public void ModificationGradateur()
+    {
+        ValeurMiseÀJour = Valeur != (int)gradateur.value; ;
+        Valeur = (int)gradateur.value;
+        txtValeur.text = Valeur.ToString();
+    }
+}
