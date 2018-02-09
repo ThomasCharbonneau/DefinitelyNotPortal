92cdc5d26b43d423e3dd270c0bf0466bf71de2c0
ThomasCharbonneau <36003308+ThomasCharbonneau@users.noreply.github.com>
2018-02-09 18:15:36 -0500

    petit bug avec les portails

--- Assets/Scripts/Portail.cs
+++ Assets/Scripts/Portail.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Portail : MonoBehaviour
 {
     public GameObject portailOpposé;
+    public GameObject portail;
 
     const float DÉLAI_PASSAGE = 0.25f;
     float TempsDepuisDernierPassage;
+    
 
     // Use this for initialization
     void Start()
     {
+        
         TempsDepuisDernierPassage = 0;
     }
 
@@ -25,14 +28,21 @@ public class Portail : MonoBehaviour
     {
         if (true) //other.tag == "personnage"
         {
-            Debug.Log("J'AI TOUCHÉ UN PORTAIL");
+            
             if (portailOpposé.activeSelf && TempsDepuisDernierPassage >= DÉLAI_PASSAGE) // S'assurer que les 2 portails sont placé avant de téléporté le joueur
             {
                 //personnage.position = (new Vector3(personnage.position.x, personnage.position.y + 20, personnage.position.z));
                 //other.transform.position = (new Vector3(other.transform.position.x, other.transform.position.y + 20, other.transform.position.z));
 
-                other.GetComponent<Rigidbody>().AddForce(Vector3.back * 500); //Donner le bon vecteur
+                /*   other.GetComponent<Rigidbody>().AddForce(Vector3.back * 500);*/ //Donner le bon vecteur
+                Vector3 vitesse = other.GetComponent<Rigidbody>().velocity;
+                Debug.Log(vitesse.magnitude);
+                vitesse = Vector3.Reflect(vitesse, portailOpposé.transform.forward);
+                vitesse = portailOpposé.transform.InverseTransformDirection(vitesse);
+                vitesse = portail.transform.TransformDirection(vitesse);
                 other.transform.position = portailOpposé.transform.position + (portailOpposé.transform.forward * 10);
+                other.GetComponent<Rigidbody>().velocity = vitesse;
+                //other.GetComponent<Rigidbody>().AddForce(vitesse.magnitude * Vector3.forward);
                 TempsDepuisDernierPassage = 0;
             }
         }
