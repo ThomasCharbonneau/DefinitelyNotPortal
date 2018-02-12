94e868c326c27f57991111cf7d22dba7d171ac6e
zacharythuotte <zacharythuotte@hotmail.com>
2018-02-12 13:42:17 -0500

    Revert "Système de mouvement repensé + jump plus réaliste"
    This reverts commit fe9ebd5574e9f806ff1a3c8a74257f6f5bf4f07c.

--- Assets/Scripts/GestionMouvement.cs
+++ Assets/Scripts/GestionMouvement.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class GestionMouvement : MonoBehaviour
 {
 
-    const float Vitesse = 10;
-    const float ForceSaut = 9f;
-    const float ForceDéplacement = 300f;
-    const float CoefficientSprint = 2.5f;
-    const float COEFFICIENT_CHUTE = 1.5F;
+    float Vitesse = 10;
+    float ForceSaut = 500f; //Plus une force qu'une vitesse? (En Newtons)
+    float ForceDéplacement = 3000f;
+    float CoefficientSprint = 2.5f;
 
     public bool EstAuSol;
 
     private Rigidbody personnage;
 
     private Vector3 déplacementAvant;
-
-    [SerializeField] Camera Caméra;
 
     [SerializeField] GameObject Sol;
 
@@ -49,44 +46,38 @@ public class GestionMouvement : MonoBehaviour
                 //personnage.AddForce(Vector3.up * VitesseSaut);
                 //personnage.velocity = new Vector3(0f, VitesseSaut, 0f);
 
-                personnage.velocity += (Vector3.up * ForceSaut);
+                personnage.AddRelativeForce(Vector3.up * ForceSaut);
 
                 EstAuSol = false;
             }
 
             if (Input.GetKey("w")) //déplacement vers l'avant
             {
-                déplacementAvant = personnage.transform.forward * Time.deltaTime * ForceDéplacement;
+                déplacementAvant = Vector3.forward * Time.deltaTime * ForceDéplacement;
 
                 if (Input.GetKey(KeyCode.LeftShift)) //Peut sprinter quand personnage dans les airs?
                 {
                     déplacementAvant *= CoefficientSprint;
                 }
 
-                personnage.velocity += déplacementAvant;
+                personnage.AddRelativeForce(déplacementAvant);
             }
 
             if (Input.GetKey("a")) //déplacement de coté vers la gauche
             {
                 //transform.Translate(Vector3.left * Time.deltaTime * Vitesse);
-                personnage.velocity += -personnage.transform.right * ForceDéplacement * Time.deltaTime;
+                personnage.AddRelativeForce(Vector3.left * ForceDéplacement * Time.deltaTime);
 
             }
 
             if (Input.GetKey("s")) //déplacement vers l'arrière
             {
-                personnage.velocity += -personnage.transform.forward * ForceDéplacement * Time.deltaTime;
+                personnage.AddRelativeForce(Vector3.back * ForceDéplacement * Time.deltaTime);
             }
 
             if (Input.GetKey("d")) //déplacement de coté vers la droite
             {
-                personnage.velocity += personnage.transform.right * ForceDéplacement * Time.deltaTime;
-                //personnage.AddRelativeForce(Vector3.right * ForceDéplacement * Time.deltaTime);
-            }
-
-            if (Input.GetKey("e")) //prendre un objet devant soi
-            {
-                PrendreObjet();
+                personnage.AddRelativeForce(Vector3.right * ForceDéplacement * Time.deltaTime);
             }
         }
         
@@ -97,14 +88,7 @@ public class GestionMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (personnage.velocity.y < 0)
-        {
-            personnage.velocity += (Vector3.up * Physics.gravity.y * (COEFFICIENT_CHUTE) * Time.deltaTime);
-        }
-        else if (personnage.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-        {
-            personnage.velocity += (Vector3.up * Physics.gravity.y * Time.deltaTime);
-        }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -113,18 +97,5 @@ public class GestionMouvement : MonoBehaviour
         {
             EstAuSol = true;
         }
-    }
-
-    private void PrendreObjet()
-    {
-        RaycastHit hit;
-        Ray ray = Caméra.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out hit);
-
-        if (hit.rigidbody.isKinematic) //Cas ou il n'y a pas de rigidbody...
-        {
-            hit.transform.position = Caméra.transform.position + transform.forward * 10;
-        }
-
     }
 }
