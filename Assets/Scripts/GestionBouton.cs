33b7e0392f0158c2c9c2828acd32fc14ad19cada
ThomasCharbonneau <36003308+ThomasCharbonneau@users.noreply.github.com>
2018-02-18 23:00:45 -0500

    Inversion de gravité

---
+++ Assets/Scripts/GestionBouton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionBouton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void InverserGravité()
+    {
+        Physics.gravity *= -1;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
