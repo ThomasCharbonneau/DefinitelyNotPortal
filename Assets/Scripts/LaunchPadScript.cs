8ad5668a6b05f5559c037aa8ee17f5fee08a1474
AjPereira04 <armandojrpereira97@hotmail.com>
2018-03-21 01:15:14 -0400

    LaunchPadAlmostDone/NeedFixMerge

--- Assets/Scripts/LaunchPadScript.cs
+++ Assets/Scripts/LaunchPadScript.cs
@@ -5,42 +5,69 @@ using UnityEngine;
 public class LaunchPadScript : MonoBehaviour {
 
     const float FORCE_Verticale = 20f;
-    const float FORCE_Horizontale = 100f;
+    const float FORCE_Horizontale = 60f;
     private Vector3 PropulsationVerticale;
     private Vector3 PropulsationHorizontale;
     private Vector3 DirectionJoueur;
     [SerializeField] AudioClip Depart;
     [SerializeField] AudioClip Launch;
     [SerializeField] Rigidbody Personnage;
+    Rigidbody personnage;
     float timer = 0.0f;
     int seconds;
     int TimerDepart;
     int LaunchTime;
     bool LAUNCH_MODE = false;
     Rigidbody CibleALaunch;
+     int i = 0;
 
     // Use this for initialization
     void Start () {
-		
+
 	}
-    void Update()
+    void FixedUpdate()
     {
         timer += Time.deltaTime;
         seconds = (int)timer % 60;
 
         if (LAUNCH_MODE)
         {
-            if(seconds == LaunchTime - 1)
+
+            if (seconds == LaunchTime - 2)
             {
-                AudioSource.PlayClipAtPoint(Depart, CibleALaunch.transform.position);
+
+                if (i == 0)
+                {
+                    AudioSource.PlayClipAtPoint(Depart, CibleALaunch.transform.position);
+                    i++;
+                }
+            }
+            if (seconds == LaunchTime - 1)
+            {
+
+                if (i == 1)
+                {
+                    AudioSource.PlayClipAtPoint(Depart, CibleALaunch.transform.position);
+                    i++;
+                }
             }
             if (seconds == LaunchTime)
             {
-                PropulsationVerticale = (Vector3.up * FORCE_Verticale * Physics.gravity.y * (-1 / 9.81f));
-
-                PropulsationHorizontale = CibleALaunch.transform.forward * FORCE_Horizontale;
-                AudioSource.PlayClipAtPoint(Launch, CibleALaunch.transform.position);
-                CibleALaunch.velocity = (PropulsationVerticale + PropulsationHorizontale);
+                if(i == 2)
+                {
+                    PropulsationVerticale = (Vector3.up * FORCE_Verticale * Physics.gravity.y * (-1 / 9.81f));
+                    if(CibleALaunch.CompareTag("Personnage"))
+                    {
+                        PropulsationHorizontale = Personnage.transform.forward * FORCE_Horizontale;
+                    }
+                    else
+                    {
+                        PropulsationHorizontale = DirectionJoueur * FORCE_Horizontale;
+                    }
+                    AudioSource.PlayClipAtPoint(Launch, CibleALaunch.transform.position);
+                    CibleALaunch.velocity = (PropulsationVerticale + PropulsationHorizontale);
+                    i++;
+                }
 
             }
         }
@@ -49,10 +76,10 @@ public class LaunchPadScript : MonoBehaviour {
     {
         Debug.Log("Entre");
         LAUNCH_MODE = true;
-        LaunchTime = seconds + 2;
-        Debug.Log("Temps Launch: "+LaunchTime);
+        LaunchTime = seconds + 3;
         CibleALaunch = other.GetComponent<Rigidbody>();
-        AudioSource.PlayClipAtPoint(Depart, CibleALaunch.transform.position);
+        DirectionJoueur = Personnage.transform.forward;
+        i = 0;
     }
     private void OnTriggerExit(Collider other)
     {
@@ -60,5 +87,4 @@ public class LaunchPadScript : MonoBehaviour {
         Debug.Log("Sort");
     }
 
-    // Update is called once per frame
 }
