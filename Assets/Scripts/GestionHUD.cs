8b8a7d6fbbb486851a6ffa4d90f323456c64d33a
AjPereira04 <armandojrpereira97@hotmail.com>
2018-03-28 19:23:07 -0400

    BugFix?

--- Assets/Scripts/GestionHUD.cs
+++ Assets/Scripts/GestionHUD.cs
@@ -77,7 +77,7 @@ public class GestionHUD : MonoBehaviour
         }
         if ("Reloader" == Choix)
         {
-            
+            VerifierMenu("Resumer");
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         if ("Resumer" == Choix)
