162737e9fd98ce85ee65e4e54dd3fb2933e8ec6d
ThomasCharbonneau <36003308+ThomasCharbonneau@users.noreply.github.com>
2018-04-04 11:28:28 -0400

    vive les portails

--- Assets/Scripts/Portail.cs
+++ Assets/Scripts/Portail.cs
@@ -14,7 +14,7 @@ public class Portail : MonoBehaviour
 
     [SerializeField] Camera caméra;
 
-    //const float DÉLAI_PASSAGE = 0.25f;
+    const float DÉLAI_PASSAGE = 1f;
     float TempsDepuisDernierPassage;
     float angle;
     public bool passéDansFrame; //Si un objet est passé dans le portail dans le frame actuel.
@@ -43,65 +43,71 @@ public class Portail : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
-        /*if (portailOpposé.activeSelf && TempsDepuisDernierPassage >= DÉLAI_PASSAGE)*/ // S'assurer que les 2 portails sont placé avant de téléporté le joueur
-        if (portailOpposé.activeSelf)
+        if(TempsDepuisDernierPassage > DÉLAI_PASSAGE)
         {
-            if (other.transform.IsChildOf(personnage.transform) && personnage.GetComponent<GestionMouvement>().TientObjet)
+            /*if (portailOpposé.activeSelf && TempsDepuisDernierPassage >= DÉLAI_PASSAGE)*/ // S'assurer que les 2 portails sont placé avant de téléporté le joueur
+            if (portailOpposé.activeSelf)
             {
-                personnage.GetComponent<GestionMouvement>().RelacherObjet();
+                if (other.transform.IsChildOf(personnage.transform) && personnage.GetComponent<GestionMouvement>().TientObjet)
+                {
+                    personnage.GetComponent<GestionMouvement>().RelacherObjet();
+                }
+
+                AudioSource.PlayClipAtPoint(SonTeleportation, portailOpposé.transform.position);
+
+                float vitesse = other.GetComponent<Rigidbody>().velocity.magnitude;
+
+
+                Vector3 normale = portailOpposé.transform.forward;
+                other.GetComponent<Rigidbody>().velocity = normale;
+
+                other.transform.position = portailOpposé.transform.position + (portailOpposé.transform.forward * 1);
+                other.GetComponent<Rigidbody>().AddForce(normale * (vitesse / Time.fixedDeltaTime), ForceMode.Acceleration);
+                Debug.Log(vitesse);
+                Debug.Log("vitesse calculé : " + (normale * vitesse / Time.fixedDeltaTime));
+
+                angle = Vector3.Angle(other.transform.forward, portailOpposé.transform.forward);
+
+                if (other.transform.eulerAngles.y > 180)
+                {
+                    angle *= -1;
+                }
+
+                if (other.name == "Personnage")
+                {
+                    scriptGestionCaméra.degréRotation = angle;
+                }
+
+
+
+
+                //other.transform.Rotate(Vector3.up * 90);
+
+
+                //other.GetComponent<Rigidbody>().rotation = Quaternion.FromToRotation(other.transform.forward, portailOpposé.transform.forward);
+
+
+
+                //La chose teleportée est placée un peu devant le portail.
+                //vitesse = Vector3.Reflect(vitesse, normale);                                                                                                              
+                //vitesse = portailOpposé.transform.InverseTransformDirection(vitesse);
+                //vitesse = portail.transform.TransformDirection(vitesse);                                                                                                         
+                //other.GetComponent<Rigidbody>().velocity = vitesse;
+                //other.GetComponent<Rigidbody>().AddForce(vitesse, ForceMode.Acceleration);
+                //float somme = Mathf.Sqrt(normale.x * normale.x + normale.y * normale.y + normale.z * normale.z);
+                //normale = new Vector3(normale.x / somme, normale.y / somme, normale.z / somme);
+                //Debug.Log(vitesse);
+                //other.GetComponent<Rigidbody>().velocity = vitesse;        
+                //Arranger la direction en sortant... Ne fonctionne pas avec :
+                //other.GetComponentInChildren<Camera>().transform.LookAt(portailOpposé.transform.position + portailOpposé.transform.forward * 2000);
+                //other.transform.LookAt(other.transform.position + vitesse * 100);
+
+                //other.GetComponent<Rigidbody>().AddForce(vitesse.magnitude * Vector3.forward);
+
+                //Debug.Break();
+                TempsDepuisDernierPassage = 0;
             }
 
-            AudioSource.PlayClipAtPoint(SonTeleportation, portailOpposé.transform.position); 
-
-            float vitesse = other.GetComponent<Rigidbody>().velocity.magnitude;
-            other.GetComponent<Rigidbody>().velocity = portailOpposé.transform.forward;
-           
-            Vector3 normale = portailOpposé.transform.forward;
-
-            other.transform.position = portailOpposé.transform.position + (portailOpposé.transform.forward * 10);
-            other.GetComponent<Rigidbody>().AddForce(normale * vitesse * 43);
-
-            angle = Vector3.Angle(other.transform.forward, portailOpposé.transform.forward);
-
-            if (other.transform.eulerAngles.y > 180)
-            {
-                angle *= -1;
-            }
-
-            if(other.name == "Personnage")
-            {
-                scriptGestionCaméra.degréRotation = angle;
-            }
-
-            Debug.Log(Vector3.Angle(other.transform.forward, portailOpposé.transform.forward));
-            Debug.Log(other.transform.eulerAngles.y);
-      
-   
-            //other.transform.Rotate(Vector3.up * 90);
-
-
-            //other.GetComponent<Rigidbody>().rotation = Quaternion.FromToRotation(other.transform.forward, portailOpposé.transform.forward);
-
-
-
-            //La chose teleportée est placée un peu devant le portail.
-            //vitesse = Vector3.Reflect(vitesse, normale);                                                                                                              
-            //vitesse = portailOpposé.transform.InverseTransformDirection(vitesse);
-            //vitesse = portail.transform.TransformDirection(vitesse);                                                                                                         
-            //other.GetComponent<Rigidbody>().velocity = vitesse;
-            //other.GetComponent<Rigidbody>().AddForce(vitesse, ForceMode.Acceleration);
-            //float somme = Mathf.Sqrt(normale.x * normale.x + normale.y * normale.y + normale.z * normale.z);
-            //normale = new Vector3(normale.x / somme, normale.y / somme, normale.z / somme);
-            //Debug.Log(vitesse);
-            //other.GetComponent<Rigidbody>().velocity = vitesse;        
-            //Arranger la direction en sortant... Ne fonctionne pas avec :
-            //other.GetComponentInChildren<Camera>().transform.LookAt(portailOpposé.transform.position + portailOpposé.transform.forward * 2000);
-            //other.transform.LookAt(other.transform.position + vitesse * 100);
-
-            //other.GetComponent<Rigidbody>().AddForce(vitesse.magnitude * Vector3.forward);
-
-            //Debug.Break();
         }
     }
     
