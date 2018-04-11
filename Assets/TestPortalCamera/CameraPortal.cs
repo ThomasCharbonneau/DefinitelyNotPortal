31dd2e635699a26830cc7e35b22429f3ad08d155
AjPereira04 <armandojrpereira97@hotmail.com>
2018-04-11 08:37:19 -0400

    Miroir

--- Assets/TestPortalCamera/CameraPortal.cs
+++ Assets/TestPortalCamera/CameraPortal.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class CameraPortal : MonoBehaviour {
 
     //https://www.youtube.com/watch?v=cuQao3hEKfs&t=467s
-
+    public Transform PlayerCamera;
+    public Transform Portal;
+    public Transform AutrePortal;
     // Use this for initialization
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void LateUpdate ()
+    {
+        //Vector3 EspaceEntreJoueur_Portal = PlayerCamera.position - AutrePortal.position;
+        //transform.position = Portal.position + EspaceEntreJoueur_Portal;
+
+        //float DifferencDeAngleRotation = Quaternion.Angle(Portal.rotation, AutrePortal.rotation);
+
+        //Quaternion DifferenceDeRotation = Quaternion.AngleAxis(DifferencDeAngleRotation, Vector3.up);
+        //Vector3 NouvelleDirectionDeCamera = DifferencDeAngleRotation * PlayerCamera.forward;
+        //transform.rotation = Quaternion.LookRotation(NouvelleDirectionDeCamera, Vector3.up);
+    }
 }
