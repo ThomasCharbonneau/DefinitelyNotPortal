c0b2cd878068934ebabad06881fb0ea87bb69a67
AjPereira04 <armandojrpereira97@hotmail.com>
2018-04-27 23:17:26 -0400

    Creation ResetZone prefab / Un peu niv 5

---
+++ Assets/Scripts/ResetZoneScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetZoneScript : MonoBehaviour
+{
+    [SerializeField] GameObject portalBleu;
+    [SerializeField] GameObject portalOrange;
+    [SerializeField] AudioClip SonDésactivationPortal;
+
+    // Use this for initialization
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (portalBleu.activeSelf || portalOrange.activeSelf)
+        {
+            portalBleu.SetActive(false);
+            portalOrange.SetActive(false);
+
+            AudioSource.PlayClipAtPoint(SonDésactivationPortal, transform.position);
+        }
+    }
+}
