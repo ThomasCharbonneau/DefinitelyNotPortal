f938bc8f7a77b2fa12b08a53cd23655e00612dc9
zacharythuotte <36003312+zacharythuotte@users.noreply.github.com>
2018-05-03 16:16:50 -0400

    Modifié pour avoir juste 1 audiosource pour la musique d'ambiance (lag moins) + un peu ajusté quelques niveaux

--- Assets/GestionMusiqueAmbiance.cs
+++ Assets/GestionMusiqueAmbiance.cs
@@ -2,32 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GestionMusiqueAmbiance : MonoBehaviour {
-    const int NOMBRE_MAX_CHANSONS = 5;
-    AudioSource[] tableauMusique;
-    int chiffreAléatoire;
+public class GestionMusiqueAmbiance : MonoBehaviour
+{
+    [SerializeField] AudioClip Musique1;
+    [SerializeField] AudioClip Musique2;
+    [SerializeField] AudioClip Musique3;
+    [SerializeField] AudioClip Musique4;
+    [SerializeField] AudioClip Musique5;
+    [SerializeField] AudioClip Musique6;
 
-	void Start () {
-		tableauMusique = GetComponents<AudioSource>();
-        for (int i = 0; i < NOMBRE_MAX_CHANSONS; ++i)
-        {
-            tableauMusique[i].volume = 0;
-        }
+    const int NOMBRE_MAX_CHANSONS = 6;
+    AudioClip[] tableauMusique;
+    AudioSource SourceAudio;
+    int chiffreAléatoire;
+    int chiffreAléatoirePrécédent;
+
+    void Start ()
+    {
+        tableauMusique = new AudioClip[6] { Musique1, Musique2, Musique3, Musique4, Musique5, Musique6 };
+		SourceAudio = GetComponent<AudioSource>();
+
+        chiffreAléatoirePrécédent = NOMBRE_MAX_CHANSONS + 1;
+
+  //      for (int i = 0; i < NOMBRE_MAX_CHANSONS; ++i)
+  //      {
+  //          tableauMusique[i].mute = true;
+  //      }
         
     }
 	// Update is called once per frame
-	void Update () {
-        if (!(tableauMusique[0].isPlaying || tableauMusique[1].isPlaying || tableauMusique[2].isPlaying || tableauMusique[3].isPlaying || tableauMusique[4].isPlaying || tableauMusique[5].isPlaying))
+	void Update ()
+    {
+        if (!(SourceAudio.isPlaying))
         {
-            chiffreAléatoire = Random.Range(0, NOMBRE_MAX_CHANSONS + 1);
-            tableauMusique[chiffreAléatoire].Play();
-            Debug.Log("Xd");
-        }
-        if (tableauMusique[0].volume < 0.1f)
-        {
-            for (int i = 0; i < NOMBRE_MAX_CHANSONS; ++i)
+            chiffreAléatoire = Random.Range(0, NOMBRE_MAX_CHANSONS);
+
+            if(chiffreAléatoire != chiffreAléatoirePrécédent)
             {
-                tableauMusique[i].volume += 0.0005f;
+                SourceAudio.clip = tableauMusique[chiffreAléatoire];
+                SourceAudio.volume = 0;
+                SourceAudio.Play();
+
+                chiffreAléatoirePrécédent = chiffreAléatoire;
+            }
+        }
+        else
+        {
+            if (SourceAudio.volume <= 0.15f)
+            {
+                SourceAudio.volume += 0.001f;
+                //for (int i = 0; i < NOMBRE_MAX_CHANSONS; ++i)
+                //{
+                //    tableauMusique[i].volume += 0.01f;
+                //}
             }
         }
     }
